76ae1714178ad2b38fbc6527917d8b6776e7725f
liush <lsh@hzdaba.com>
2018-08-30 19:13:46 +0800

    fix

--- zeus-web/src/test/java/org/com/zeus/PersonTest.java
+++ zeus-web/src/test/java/org/com/zeus/PersonTest.java
@@ -12,12 +12,16 @@ public class PersonTest extends AppTest {
 	PersonMapper personDao;
 
 	@Test
-	public void test() {
+	public <T> void test() {
 		Person person=new Person();
 		person.setName("张三");
 		personDao.insert(person);
-		System.out.println(personDao.selectPage(new Page<Person>(1, 2), null));
 		System.out.println(personDao.selectById(1L));
 		System.out.println(personDao.cgetAllName());
+		Page<T> pagePlus = new Page<T>();
+		pagePlus.setCurrent(0);
+        pagePlus.setSize(10);
+        pagePlus.setAsc("id");
+		System.out.println(personDao.selectPage(new Page<Person>(1, 10), null));
 	}
 }
